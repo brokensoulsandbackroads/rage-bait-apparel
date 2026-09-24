a6a097f76f3abe31edd0567046baacb3372675a6
brokensoulsandbackroads <brokensoulsandbackroads@gmail.com>
2026-09-24 19:24:03 +0100

    Load Virtual Degenerate Hoodie product script

--- app.js
+++ app.js
@@ -5,7 +5,14 @@
 
   const script=document.createElement('script');
   script.src=`app-core.js${version}`;
-  script.onload=()=>document.dispatchEvent(new CustomEvent('ragebait:ready'));
+  script.onload=()=>{
+    document.dispatchEvent(new CustomEvent('ragebait:ready'));
+
+    const hoodieScript=document.createElement('script');
+    hoodieScript.src=`virtual-degenerate.js${version}`;
+    hoodieScript.onerror=()=>console.error('Virtual Degenerate Hoodie failed to initialise');
+    document.body.appendChild(hoodieScript);
+  };
   script.onerror=()=>console.error('Rage Bait app failed to initialise');
   document.body.appendChild(script);
 })();
